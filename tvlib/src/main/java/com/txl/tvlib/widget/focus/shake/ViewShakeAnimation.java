bbd3dab583930e028ee89e6f2848a06ba1f650e2
xiaoluTang <2935347599@qq.com>
2020-03-18 22:28:44 +0800

    title(优化):优化焦点寻找的抖动效果

--- tvlib/src/main/java/com/txl/tvlib/widget/focus/shake/ViewShakeAnimation.java
+++ tvlib/src/main/java/com/txl/tvlib/widget/focus/shake/ViewShakeAnimation.java
@@ -13,7 +13,7 @@ public class ViewShakeAnimation implements IFocusShake{
     //定义一个横向抖动的动画
     private ObjectAnimator translationAnimatorX;
     private ObjectAnimator translationAnimatorY;
-    private static final int DURATION = 300;
+    private static final int DURATION = 500;
 
     /**
      * 抖动偏差 单位px
@@ -40,7 +40,7 @@ public class ViewShakeAnimation implements IFocusShake{
 
     private void setVerticalShakeAnimator(View newView) {
         //动画种类：X轴平移，后面的值为移动参数，正值为右，负值为左（Y轴正值为下，负值为上）
-        translationAnimatorY = ObjectAnimator.ofFloat(newView, "translationY", 0f, offset, 0f, -offset, 0f, offset, 0f, -offset, 0f);
+        translationAnimatorY = ObjectAnimator.ofFloat(newView, "translationY", 0f, offset, 0f, -offset, 0f);
         //用于控制动画快慢节奏，此处使用系统自带的线性Interpolator（匀速），此外还有各种变速Interpolator
         translationAnimatorY.setInterpolator(new LinearInterpolator());
         //设置动画重复次数，ValueAnimator.INFINITE即-1表示用于一直重复
@@ -51,7 +51,7 @@ public class ViewShakeAnimation implements IFocusShake{
 
     private void setHorizontalShakeAnimator(View newView) {
         //动画种类：X轴平移，后面的值为移动参数，正值为右，负值为左（Y轴正值为下，负值为上）
-        translationAnimatorX = ObjectAnimator.ofFloat(newView, "translationX", 0f, offset, 0f, -offset, 0f, offset, 0f, -offset, 0f);
+        translationAnimatorX = ObjectAnimator.ofFloat(newView, "translationX", 0f, offset, 0f, -offset, 0f);
         //用于控制动画快慢节奏，此处使用系统自带的线性Interpolator（匀速），此外还有各种变速Interpolator
         translationAnimatorX.setInterpolator(new LinearInterpolator());
         //设置动画重复次数，ValueAnimator.INFINITE即-1表示用于一直重复
